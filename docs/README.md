62607c05bc2cfa726895f155c71fd9a636dfd2f0
Kolen Cheung <christian.kolen@gmail.com>
2021-08-08 10:59:53 -0700

    refactor download.py and update README.md to have a smaller table

--- docs/README.md
+++ docs/README.md
@@ -4,12 +4,27 @@ title: Selected Hymns
 
 Selected Hymns (詩歌選集) is a hymnal released by Tree of Life Publishers in 1995, which is later dicontinued due to copyright issue. This project is to provide a text-only version of the original hymnal. The long-term goal will be to identify the texts that has copyright issue and replaces it with one that is not.
 
-You can see the texts in
+# Install
+
+## Recommended
+
+Mobile phone users should download from the ePub column. Apple users can open it using Apple Books, and Android users using Google Play Books.
+
+Choose the row according to what you can read. If you read Chinese, choose the Chinese only version or bilingual version according to your own preference.
 
 ```table
 ---
 markdown: true
 include: docs/download.csv
+...
+```
+
+## All formats
+
+```table
+---
+markdown: true
+include: docs/download-all.csv
 ...
 ```
 
--- docs/download.py
+++ docs/download.py
@@ -3,13 +3,17 @@
 
 Should be run from root where the output path is hard coded to docs/download.csv
 '''
+from __future__ import annotations
+
 import os
-from collections import OrderedDict
+from typing import List
+from pathlib import Path
 
 from github import Github
 import pandas as pd
+import defopt
 
-versions = {
+VERSIONS = {
     'en': 'English',
     'zh-Hant': '繁體中文版',
     'zh-Hant-en': '繁體中英對照',
@@ -17,8 +21,8 @@ versions = {
     'zh-Hans-en': '简体中英对照',
 }
 
-formats = {
-    '.epub': 'ePub^[for mobile such as Apple Books or Google Play Books]',
+FORMATS = {
+    '.epub': 'ePub',
     '.pdf': 'PDF',
     '-logos.docx': 'Logos version',
     '.html': 'HTML (browser)',
@@ -31,7 +35,11 @@ def get_url(url_dict, format, version):
     return f'[{name}]({url})'
 
 
-if __name__ == "__main__":
+def main(
+    versions: List[str] = list(VERSIONS.keys()),
+    formats: List[str] = list(FORMATS.keys()),
+    output: Path = Path('docs/download-all.csv'),
+):
     g = Github(os.environ['GITHUB_TOKEN'])
     repo = g.get_repo('ickc/selected-hymns')
     release = repo.get_latest_release()
@@ -39,8 +47,16 @@ if __name__ == "__main__":
 
     df = pd.DataFrame(
         [[get_url(url_dict, format, version) for format in formats] for version in versions],
-        index=versions.values(),
-        columns=formats.values()
+        index=[VERSIONS[version] for version in versions],
+        columns=[FORMATS[format] for format in formats],
     )
 
-    df.to_csv('docs/download.csv')
+    df.to_csv(output)
+
+
+def cli():
+    defopt.run(main, strict_kwonly=False)
+
+
+if __name__ == "__main__":
+    cli()
