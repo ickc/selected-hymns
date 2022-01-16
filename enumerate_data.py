3aa1fa136cb01c5db8a972bd2ff6fea31d0a02cf
Kolen Cheung <christian.kolen@gmail.com>
2022-01-16 11:57:34 -0800

    add enumerate_data.*

---
+++ enumerate_data.py
@@ -0,0 +1,57 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.6
+#   kernelspec:
+#     display_name: all39-conda-forge
+#     language: python
+#     name: all39-conda-forge
+# ---
+
+# %%
+import yaml
+import yamlloader
+
+# %% [markdown]
+# To
+
+# %%
+with open("data.yml", "r") as f:
+    data = yaml.load(f, Loader=yamlloader.ordereddict.CSafeLoader)
+
+# %%
+data_enum = {i: datum for i, datum in enumerate(data, 1)}
+
+# %%
+with open("data-enum.yml", "w") as f:
+    yaml.dump(
+        data_enum,
+        f,
+        Dumper=yamlloader.ordereddict.CSafeDumper,
+        default_flow_style=False,
+        allow_unicode=True,
+    )
+
+# %% [markdown]
+# From
+
+# %%
+with open("data-enum.yml", "r") as f:
+    data_enum = yaml.load(f, Loader=yamlloader.ordereddict.CSafeLoader)
+
+# %%
+data = list(data_enum.values())
+
+# %%
+with open("data.yml", "w") as f:
+    yaml.dump(
+        data,
+        f,
+        Dumper=yamlloader.ordereddict.CSafeDumper,
+        default_flow_style=False,
+        allow_unicode=True,
+    )
